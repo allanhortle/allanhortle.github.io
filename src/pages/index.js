f24c81aed7493f877f35131d7a228bb80dae4a55
Allan Hortle <allanhortle@gmail.com>
2018-02-17 17:39:09 +1100

    Fix package add magnitude

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Link from "gatsby-link";
 
-export default () => <div className="Main">
+export default (props) => <div className="Main">
     <h1>Allan Hortle</h1>
     <ul className="List">
         <li><Link to="/spoon">Spoon</Link></li>
         <li><Link to="/decimal-time">Decimal Time</Link></li>
-        <li><a href="/chord/">Chords</a></li>
+        <li><a href="/magnitude/">Magnitude</a></li>
+        {/*<li><a href="/chord/">Chords</a></li>*/}
+        <li><a href="https://bible.allanhortle.com">Bible Word Stats</a></li>
         {/*<li><a href="/posdnuos">Posdnuos</a></li>*/}
     </ul>
 </div>
